fdbc846dc9862a91ee23b1cf2340ad411ce16724
thepythonistdeveloper <fakhriddindeveloper@gmail.com>
2022-04-21 20:32:52 +0500

    setup db src

--- src/settings.py
+++ src/settings.py
@@ -37,10 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
     'db',
     'api',
-    'rest_framework'
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+      #      'rest_framework_simplejwt.authentication.JWTAuthentication',
+         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
+
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

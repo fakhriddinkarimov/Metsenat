58423cf3bd66717112244e1d80cbe5ffd73e897c
thepythonistdeveloper <fakhriddindeveloper@gmail.com>
2022-04-21 20:30:55 +0500

    api urls

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from .views import Sponsor_list,Sponsor_detail,Student_list,Student_detail, \
- SponsorShip_list,SponsorShip_detail,University_list,University_detail,SponsorshipsByStudentView,SponsorshipsBySponsorView,DashboardView,Dashboard
+ SponsorShip_list,SponsorShip_detail,University_list,University_detail,Sponsorships_Student,Sponsorships_Sponsor,DashboardView,Dashboard
 
 urlpatterns = [
     path('sponsor',Sponsor_list.as_view(),name='sponsor-list'),
     path('sponsor/<int:pk>', Sponsor_detail.as_view(), name='sponsor'),
-    path('sponsor/<int:pk>/sponsorships', SponsorshipsBySponsorView.as_view(), name='sponsor-sponsorships'),
+    path('sponsor/<int:pk>/sponsorships', Sponsorships_Sponsor.as_view(), name='sponsor-sponsorships'),
 
     path('students', Student_list.as_view(), name='student-list'),
     path('students/<int:pk>', Student_detail.as_view(), name='student'),
-    path('students/<int:pk>/sponsorships', SponsorshipsByStudentView.as_view(), name='student-sponsorships'),
+    path('students/<int:pk>/sponsorships', Sponsorships_Student.as_view(), name='student-sponsorships'),
 
     path('sponsorship', SponsorShip_list.as_view(), name='sponsorship-list'),
     path('sponsorship/<int:pk>', SponsorShip_detail.as_view(), name='sponsorship'),

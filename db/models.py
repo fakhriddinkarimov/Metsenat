97faf98261f50d244ca4368914df8097e204f11b
thepythonistdeveloper <fakhriddindeveloper@gmail.com>
2022-04-21 20:14:24 +0500

    models update

--- db/models.py
+++ db/models.py
@@ -1,54 +1,61 @@
 from re import M, T
 from statistics import mode
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
+
 class Sponsor(models.Model):
-    Status = (
+    status_choices = (
         ('Yangi', 'Yangi'),
         ('Moderatsiya','Moderatsiya'),
-        ('Tasdiqlangan','Tasdiqlangan')
+        ('Tasdiqlangan','Tasdiqlangan'),
         ('Bekor qilingan','Bekor qilingan')
     )
 
-    Sponsor_Status(
+    sponsor_choices = (
         ('Jsh','Jismoniy shaxs'),
         ('Ysh','Yuridik shaxs')
     )
 
     first_name = models.CharField(max_length=32,blank=False,null=False)
     last_name = models.CharField(max_length=32,blank=False,null=False)
-    SponsorStatus = models.CharField(max_length=24,choices=SponsorStatus,default='Jsh',null=False,blank=False)
+    sponsor_status = models.CharField(max_length=24,choices=sponsor_choices,default='Jsh')
     company = models.CharField(max_length=64,null=True)
     money = models.IntegerField(null=False,blank=False)
-    status = models.CharField(max_length=32,choices=Status,default='Yangi')
+    status = models.CharField(max_length=32,choices=status_choices,default='Yangi')
     email = models.CharField(max_length=64,blank=False,null=False)
-    crated_dt = models.DateField(auto_now_add=True)
+    created_dt = models.DateField(auto_now_add=True)
 
 
     def __str__(self):
         return f"{self.first_name}   {self.last_name}"
 
 
-class Univer(models.Model):
+class University(models.Model):
     name = models.CharField(max_length=128,blank=False,null=False)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'University'
+        verbose_name_plural = 'Universities'
+
 
 class Student(models.Model):
-    Degree = (
+    degree_choices = (
         ('Bakalavr','Bakalavr'),
         ('Magistr','Magistr'),
     )
 
     first_name = models.CharField(max_length=32,blank=False,null=False)
     last_name = models.CharField(max_length=32,blank=False,null=False)
-    unversity = models.ForeignKey(Univer,on_delete=models.CASCADE)
-    degree = models.CharField(max_length=32,blank=False,null=False)
-    contract =models.IntegerField(null=False)
+    university = models.ForeignKey(University,on_delete=models.CASCADE)
+    degree = models.CharField(max_length=32,choices=degree_choices,default='Bakalavr', blank=False)
+    course = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(4)])
+    contract = models.IntegerField(null=False)
     phone_number = models.CharField(max_length=14,null=False,blank=False)
     email = models.CharField(max_length=64,blank=False,null=False)
     created_dt = models.DateField(auto_now_add=True)
@@ -60,13 +67,16 @@ class Student(models.Model):
 
 
 class SponsorShip(models.Model):
-    sponsor = models.ForeignKey(Sponsor,on_delete=models.CASCADE)
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    sponsor = models.ForeignKey(Sponsor,related_name='sponsorships',on_delete=models.CASCADE)
+    student = models.ForeignKey(Student,related_name='sponsorships',on_delete=models.CASCADE)
     money = models.IntegerField(null=False,blank=False)
     created_dt = models.DateField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True)
 
 
     def __str__(self) -> str:
-        return f"{self.sponsor.last_name} {self.sponsor.last_name} -- {self.student.first_name} {self.student.last_name}"
-        
+        return f"Sponsor :{self.sponsor.last_name} {self.sponsor.last_name} -> Student {self.student.first_name} {self.student.last_name}"
+
+    class Meta:
+        verbose_name = 'Sponsorship'
+        verbose_name_plural = 'Sponsorships'
